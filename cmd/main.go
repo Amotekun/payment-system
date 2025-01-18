736b1d59de16fdabb8bfd3840fa122775a467245
Elijah <elijahajayi444@gmail.com>
2025-01-18 07:26:00 -0800

    feat: implement user registration handler with bcrypt hashing and Gorilla/mux routing

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/Amotekun/payment-system/internal/config"
+import (
+	"log"
+	"net/http"
+
+	"github.com/Amotekun/payment-system/internal/config"
+	"github.com/Amotekun/payment-system/internal/handlers"
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	config.InitDB()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/api/register", handlers.Register).Methods("POST")
+
+	log.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", router)
 }
